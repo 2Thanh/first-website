3cfaca24058a98dbfdfd782989366a7ce72401b9
2Thanh <88953341+2Thanh@users.noreply.github.com>
2022-03-01 09:41:25 +0700

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
+from turtle import down
 from flask import Blueprint, render_template,request,flash,redirect,url_for,request
 from flask_login import login_required, current_user
 from . import db
-from .models import User,Post,Comment,Like,Dislike
-from werkzeug.utils import secure_filename
+from .models import User,Post,Comment,Like,Dislike,Images
+from werkzeug.utils import secure_filename,send_from_directory
 from __init__ import app
 import os
 
@@ -13,8 +14,9 @@ views = Blueprint("views", __name__)
 def home():
     print(current_user)
     posts=Post.query.all()
+    imgs=Images.query.all()
     comments=Comment.query.all()
-    return render_template("home.html", user=current_user, posts=posts, comment=comments)
+    return render_template("home.html", user=current_user, posts=posts, comment=comments, imgs=imgs)
     
 @views.route("/create_post", methods=["GET","POST"])
 @login_required
@@ -56,8 +58,6 @@ def posts(username):
 @views.route("/create-comment/<post_id>",methods=["POST"])
 @login_required
 def comment_create(post_id):
-        print("cai nay la",post_id)
-        print("Tac gia la",current_user.id)
         text=request.form.get("text")
         if not text:
             flash("Comment cannot be empty", category="error")
@@ -70,6 +70,17 @@ def comment_create(post_id):
             else:
                 flash("post not found",category="error")
         return redirect(url_for("views.home"))
+
+
+UPLOAD_FOLDER = 'website/static/uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+@views.route("/post_img")
+@login_required
+def post_img():
+    imgs=Images.query.all()
+    return render_template("post_img.html", imgs=imgs, user=current_user)
+
 @views.route("/delete_comments/<id>")
 @login_required
 def delete_comment(id):
@@ -85,41 +96,80 @@ def delete_comment(id):
     return redirect(url_for("views.home"))
 
 
-@views.route('/post_image')
-@login_required
+@views.route('/display_image')
 def upload_form():
-	return render_template('post_image.html')
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+	return render_template('display_image.html', user=current_user)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 	
-@views.route('/post_image', methods=['POST'])
-def upload_image():
-	if 'file' not in request.files:
-		flash('No file part', category='error')
-		return redirect(request.url)
-	file = request.files['file']
+@views.route('/display_image/<id>', methods=['POST'])
+def upload_image(id):
+    posts=Post.query.filter_by(id=id).first()
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part', category='error')
+            return redirect(request.url)
+            #request.url là yêu cầu url hiện tại
+        file = request.files['file']
+        
+        if file.filename == '':
+            flash('No image selected for uploading')
+            return redirect(request.url)   
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            #save file vào thư mục uploads
+            images=Images.query.filter_by(image=filename).first()
+            posts=Post(text=filename,author=id)
+            db.session.add(posts)
+            if not images:
+                images=Images(image=filename,author=id)
+                db.session.add(images)
+            db.session.add(posts)
+            db.session.commit()
+            print('upload_image filename: ' + filename)
+            flash('Image successfully uploaded and displayed below',category="error")
+            #return render_template('display_image.html',user=current_user,images=images)
+            return redirect(url_for('views.home'))
+        else:
+            flash('Allowed image types are -> png, jpg, jpeg, gif')
+            return redirect(request.url)
 
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect(request.file)
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		print('upload_image filename: ' + filename)
-		flash('Image successfully uploaded and displayed below',category="error")
-		return render_template('upload.html', filename=filename)
-	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif')
-		return redirect(request.url)
-    
 
-@views.route('/post_image/<filename>')
-def post_image(filename):
-	flash("Image successfully uploaded and displayed below", category="error")
-	return redirect(url_for('static', filename= "uploads/"+filename), code=301)
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @views.route('/like_post/<post_id>')
 @login_required
